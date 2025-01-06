5f059aa72d606cb3a1c3bc506febe10368f8489f
vargheesk <vargheeskuttyeldhose@gmail.com>
2025-01-06 12:28:38 +0530

    empty input error handle added

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -6,36 +6,37 @@ import '../pages/home/home.dart';
 import '../pages/login/login.dart';
 
 class AuthService {
-
-  Future<void> signup({
-    required String email,
-    required String password,
-    required BuildContext context
-  }) async {
-    
-    try {
-
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password
+  Future<void> signup(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    if (email.isEmpty || password.isEmpty) {
+      Fluttertoast.showToast(
+        msg: 'Email and password cannot be empty.',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
       );
+      return;
+    }
+
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
 
       await Future.delayed(const Duration(seconds: 1));
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) => const Home()
-        )
-      );
-      
-    } on FirebaseAuthException catch(e) {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) => const Home()));
+    } on FirebaseAuthException catch (e) {
       String message = '';
       if (e.code == 'weak-password') {
         message = 'The password provided is too weak.';
       } else if (e.code == 'email-already-in-use') {
         message = 'An account already exists with that email.';
       }
-       Fluttertoast.showToast(
+      Fluttertoast.showToast(
         msg: message,
         toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.SNACKBAR,
@@ -43,42 +44,49 @@ class AuthService {
         textColor: Colors.white,
         fontSize: 14.0,
       );
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: 'An unexpected error occurred.',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
+      );
     }
-    catch(e){
-
-    }
-
   }
 
-  Future<void> signin({
-    required String email,
-    required String password,
-    required BuildContext context
-  }) async {
-    
-    try {
-
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password
+  Future<void> signin(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    if (email.isEmpty || password.isEmpty) {
+      Fluttertoast.showToast(
+        msg: 'Email and password cannot be empty.',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
       );
+      return;
+    }
+
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
 
       await Future.delayed(const Duration(seconds: 1));
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) => const Home()
-        )
-      );
-      
-    } on FirebaseAuthException catch(e) {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) => const Home()));
+    } on FirebaseAuthException catch (e) {
       String message = '';
       if (e.code == 'invalid-email') {
         message = 'No user found for that email.';
       } else if (e.code == 'invalid-credential') {
         message = 'Wrong password provided for that user.';
       }
-       Fluttertoast.showToast(
+      Fluttertoast.showToast(
         msg: message,
         toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.SNACKBAR,
@@ -86,24 +94,22 @@ class AuthService {
         textColor: Colors.white,
         fontSize: 14.0,
       );
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: 'An unexpected error occurred.',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
+      );
     }
-    catch(e){
-
-    }
-
   }
 
-  Future<void> signout({
-    required BuildContext context
-  }) async {
-    
+  Future<void> signout({required BuildContext context}) async {
     await FirebaseAuth.instance.signOut();
     await Future.delayed(const Duration(seconds: 1));
     Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) =>Login()
-        )
-      );
+        context, MaterialPageRoute(builder: (BuildContext context) => Login()));
   }
 }
